e3c0f1db44470729f91e9ce8acdcc2ccc3c7f123
Tim Paine <t.paine154@gmail.com>
2019-12-27 14:41:33 -0500

    working on #4

--- setup.py
+++ setup.py
@@ -1,31 +1,42 @@
-# # # # GENERATED FILE -- DO NOT MODIFY # # # #
 from setuptools import setup
 from codecs import open
-from os import path
+import io
+import os
+import os.path
 
-here = path.abspath(path.dirname(__file__))
+pjoin = os.path.join
+here = os.path.abspath(os.path.dirname(__file__))
+name = 'pyEX.caching'
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+
+def get_version(file, name='__version__'):
+    path = os.path.realpath(file)
+    version_ns = {}
+    with io.open(path, encoding="utf8") as f:
+        exec(f.read(), {}, version_ns)
+    return version_ns[name]
+
+version = get_version(pjoin(here, 'pyEX', 'caching', '_version.py'))
+
+with open(pjoin(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+with open(pjoin(here, 'requirements.txt'), encoding='utf-8') as f:
     requires = f.read().split()
 
 setup(
-    name='pyEX.caching',
-    version='0.0.3',
+    name=name,
+    version=version,
     description='Rest API to IEX',
     long_description=long_description,
-    url='https://github.com/timkpaine/pyEX-caching',
-    download_url='https://github.com/timkpaine/pyEX-caching/archive/v0.0.3.tar.gz',
+    long_description_content_type='text/markdown',
+    url='https://github.com/timkpaine/{name}'.format(name=name),
     author='Tim Paine',
-    author_email='timothy.k.paine@gmail.com',
+    author_email='t.paine154@gmail.com',
     license='Apache 2.0',
 
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
@@ -33,7 +44,8 @@ setup(
     ],
 
     keywords='finance data',
+    zip_safe=False,
     packages=['pyEX.caching'],
     install_requires=requires,
-    extras_require={'dev': requires + ['pytest', 'pytest-cov', 'pylint', 'flake8', 'mock', 'autopep8', 'codecov']}
+    extras_require={'dev': requires + ['pytest', 'pytest-cov', 'pylint', 'flake8', 'codecov', 'bumpversion', 'mock']}
 )
